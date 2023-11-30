4e514a56229851f42da8a4bcf28ed8d9dff9c928
Alberto Ricatto s309141 <s309141@studenti.polito.it>
2023-11-30 17:35:18 +0100

    finally implemented a GA

--- lab9/GA.py
+++ lab9/GA.py
@@ -2,41 +2,53 @@ import numpy
 from copy import deepcopy
 from random import random, randint, choices
 
-def individual(self, genome: numpy.array, fitness):
-    self.genome = genome
-    self.fitness = fitness(self.genome)
+class Individual:
+    def __init__(self, genome: numpy.array, fitness):
+        self.genome = genome
+        self.fitness_function = fitness
+        self.fitness = 0
 
-    def mutate(N: int) -> None:
+    def mutate(self, N: int) -> None:
         """mutate N random bits in the sequence"""
         for _ in range(N):
             i = randint(0, 999)
             self.genome[i] = 1 - self.genome[i]
 
+    def compute_fitness(self) -> None:
+        self.fitness = self.fitness_function(self.genome)
 
-def GA(self, population_size, fitness, N):
-    self.population = []   
-    self.fitness = fitness
-    self.N = N
-    for _ in range(population_size):
-        self.population.append(individual(choices([0, 1], k=1000)), fitness, N) 
 
-    def select_by_tournament(tournament_size: int) -> individual:
+
+class Ga:
+    def __init__(self, population_size: int, fitness, N: int):
+        self.population = []  
+        self.fitness = fitness
+        self.N = N
+        self.population_size = population_size
+        for _ in range(population_size):
+            i = Individual(numpy.array(choices([0, 1], k=1000)), self.fitness)
+            i.compute_fitness()
+            self.population.append(i) 
+
+    def select_by_tournament(self, tournament_size: int) -> Individual:
         selected = []
         for _ in range(tournament_size):
-            selected.append(self.population[randint(0, 999)])  
+            selected.append(self.population[randint(0, self.population_size - 1)])  
         return max(selected, key = lambda a: a.fitness)  
 
-    def generate_offspring(offspring_size: int, pm: float, tournament_size: int):  
+    def generate_offspring(self, offspring_size: int, pm: float, tournament_size: int):  
         """selects parents using tournament size pm,
             then generates offspring_size new individuals
             that can mutate with probability pm"""
-        
-        p1, p2 = (select_by_tournament(tournament_size), select_by_tournament(tournament_size))
-        g1, g2 = deepcopy(p1.genome), deepcopy(p2.genome)
-
-        for _ in range(offspring_size):
-            cut = randint(0, 999)
-            new_individual = individual(numpy.concatenate(g1[0:cut], g2[cut:]), self.fitness, self.N)
+        parents = [self.select_by_tournament(tournament_size) for _ in range(offspring_size * 2)]
+        for i in range(offspring_size):
+            cut = randint(0, 999) 
+            new_individual = Individual(numpy.concatenate((parents[i * 2].genome[:cut], parents[i * 2 + 1].genome[cut:])), self.fitness)
             if random() < pm:
                 new_individual.mutate(self.N)
+            new_individual.compute_fitness()    
             self.population.append(new_individual)
+
+    def survival_selection(self, population_size: int):
+        self.population.sort(key= lambda i: i.fitness, reverse=True)
+        self.population = self.population[0:population_size]        
